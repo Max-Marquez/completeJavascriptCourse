b3a25bca3cf6695bbf5ba96da43896eb12ff09d9
Max Marquez <maxmarquezjr@gmail.com>
2019-01-08 17:35:49 -0700

    Completed Part 2 of Code Challenge 5
    Completed Part 2 of Code Challenge 5
    Started Section 3

--- script.js
+++ script.js
@@ -610,5 +610,56 @@ var john = {
   }
 };
 
+var mark = {
+  fullName: "Mark Miller",
+  bills: [77, 375, 110, 45],
+  tipCalc: function() {
+    this.tips = [];
+    this.finalValues = [];
+    for (var i = 0; i < this.bills.length; i++) {
+      // Determine percentage based on tipping rules
+      var percentage;
+      var bill = this.bills[i];
+      if (bill < 100) {
+        percentage = 0.2;
+      } else if (bill >= 100 && bill < 300) {
+        percentage = 0.1;
+      } else {
+        percentage = 0.25;
+      }
+      // Add results to corresponding arrays
+      this.tips[i] = bill * percentage;
+      this.finalValues[i] = bill + bill * percentage;
+    }
+  }
+};
+
+function calcAverage(tips) {
+  var sum = 0;
+  for (var i = 0; i < tips.length; i++) {
+    sum = sum + tips[i];
+  }
+  return sum / tips.length;
+}
+
+// Do the calculations
 john.tipCalc();
-console.log(john);
+mark.tipCalc();
+
+john.average = calcAverage(john.tips);
+mark.average = calcAverage(mark.tips);
+console.log(john, mark);
+
+if (john.average > mark.average) {
+  console.log(
+    john.fullName +
+      "'s family pays higher tips, with an average of $" +
+      john.average
+  );
+} else if (john.average < mark.average) {
+  console.log(
+    mark.fullName +
+      "'s family pays higher tips, with an average of $" +
+      mark.average
+  );
+}
