c460be94f359f88074a1fbebee8595a6287d980c
Max Marquez <maxmarquezjr@gmail.com>
2019-01-12 18:25:26 -0700

    Dom manipulation and CSS styling using JavaScript

--- 4-DOM-pig-game/app.js
+++ 4-DOM-pig-game/app.js
@@ -16,4 +16,10 @@ activePlayer = 0;
 dice = Math.floor(Math.random() * 6) + 1;
 console.log(dice);
 
-document.querySelector("#score-0").textContent = dice;
+document.querySelector("#current-" + activePlayer).textContent = dice;
+//document.querySelector("#current-" activePlayer).innerHTML = '<em>' + dice + "</em>"
+
+var x = document.querySelector("#score-0").textContent;
+console.log(x);
+
+document.querySelector(".dice").style.display = "none";
